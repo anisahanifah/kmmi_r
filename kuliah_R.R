3e38124e368270a896321af8981187221ff80a2f
anisahanifah <hanifaha747@gmail.com>
2021-08-11 22:03:49 +0700

    menambahkan baris baru

--- kuliah_R.R
+++
@@ -1,8 +0,0 @@
-teks1 = "semat datang didunia tipu tipu"
-teks2 = "anda akan takjub didalam duniannya"
-
-teks1 
-teks2
-
-list1 = list("soto ayam", "ayam goreng", "sate kambing")
-list 
--- teks.r
+++ teks.r
@@ -1,1 +1,2 @@
 teks= "haloo selamat datang di R"
+a= "matahari dan pelangi"
